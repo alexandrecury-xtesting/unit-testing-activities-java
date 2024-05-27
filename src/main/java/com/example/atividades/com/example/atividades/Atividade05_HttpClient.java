bbf2ba9e1fd7965c01bcc6a5958104f2cbc0fcce
Alexandre Cury <alexandrecurylima@hotmail.com>
2024-05-27 13:45:30 -0300

    fix project

--- src/main/java/com/example/atividades/com/example/atividades/Atividade05_HttpClient.java
+++ src/main/java/com/example/atividades/com/example/atividades/Atividade05_HttpClient.java
@@ -6,9 +6,13 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class Atividade05_HttpClient {
-    private final OkHttpClient client = new OkHttpClient();
+    private OkHttpClient client;
 
-    public String getDataFromApi(String url) throws IOException {
+    public Atividade05_HttpClient(OkHttpClient okHttpClient) {
+		 client = okHttpClient;
+	}
+
+	public String getDataFromApi(String url) throws IOException {
         Request request = new Request.Builder().url(url).build();
         try (Response response = client.newCall(request).execute()) {
             return response.body().string();
